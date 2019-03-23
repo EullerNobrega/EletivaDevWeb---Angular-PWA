fcc08b3f69559073d8e209515c54e3b97e8e3ab4
Euller Nóbrega <eullernobrega@gmail.com>
2019-03-22 21:58:26 -0300

    Deploy no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAm_c9UGl0hwrDdjPlr_nnUJl6gAOyeBlg",
+    authDomain: "disciplinasunicap.firebaseapp.com",
+    databaseURL: "https://disciplinasunicap.firebaseio.com",
+    projectId: "disciplinasunicap",
+    storageBucket: "disciplinasunicap.appspot.com",
+    messagingSenderId: "914752118789"
+  }
 };
